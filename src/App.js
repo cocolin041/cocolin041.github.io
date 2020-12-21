e405aa48fe7727cd559b534d0fbb878617814ca1
SihYi Lin <sihyiyl2@illinois.edu>
2020-12-21 02:01:49 -0600

    ✨ Feat: Finish landing page and navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import bg from './assets/bg.jpg'
+import avatar from './assets/avatar.jpg'
+import logo from './assets/logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+        <nav>
+          <div className='website-name'><a href='#'>Coco Lin</a></div>
+          <div className='menu'>
+            <ul>
+              <li><a href='#about'>ABOUT</a></li>
+              <li><a href='#skill'>WEB SKILL</a></li>
+              <li><a href='#education'>EDUCATION</a></li>
+              <li><a href='#experience'>EXPERIENCE</a></li>
+              <li><a href='#portfolio'>PORTFOLIO</a></li>
+            </ul>
+          </div>
+        </nav>
+
+      
+
+      <section id='landing-page'>
+        {/* <img src={bg} className="background-img" alt="background" /> */}
+      </section>
+
+      <section id='about'>
+        <div className='container'>
+          <div className="avatar"><img src={avatar} alt="avatar"/></div>
+
+        </div>
+      </section>
+
+      <section id='skill'>
+      </section>
+
+      <section id='education'>
+      </section>
+
+      <section id='experience'>
+      </section>
+
+      <section id='portfolio'>
+      </section>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
